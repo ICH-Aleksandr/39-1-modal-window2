af127a9968b9bb937bb0f98e72bcfcf591a62c04
Aleksandr <ichmarchuk@gmail.com>
2025-12-17 21:48:41 +0100

    new commit

--- scripts/script.js
+++ scripts/script.js
@@ -23,3 +23,99 @@ document.addEventListener("keydown", (e) => {
 // закрытие через крестик:
 const closeBtn = document.querySelector(".closeBtn");
 closeBtn.addEventListener("click", toggleModalWindow);
+
+//////////////////////////////////////////////
+//  form //
+const registerForm = document.querySelector("#registerForm");
+const usernameInput = document.querySelector(".username");
+const lastnameInput = document.querySelector(".lastname");
+const usernameLabel = document.querySelector(".usernameLabel");
+const lastnameLabel = document.querySelector(".lastnameLabel");
+
+const loginInput = document.querySelector(".login");
+const loginLabel = document.querySelector(".loginLabel");
+const passwordInput = document.querySelector(".password");
+const passwordLabel = document.querySelector(".passwordLabel");
+
+function renderStatusMessage(parent, data) {
+  if (parent.lastElementChild.nodeName === "P") {
+    parent.lastElementChild.remove();
+  }
+  const statusMessageElement = document.createElement("p");
+  parent.appendChild(statusMessageElement);
+  statusMessageElement.textContent = data.text;
+  statusMessageElement.style.color = data.color;
+}
+
+function usernameValidation(value) {
+  return value.length >= 2 && value.length <= 26
+    ? {
+        text: "valid data",
+        color: "green",
+      }
+    : {
+        text: "invalid data",
+        color: "red",
+      };
+}
+
+function lastnameValidation(value) {
+  return value.length >= 2 && value.length <= 30
+    ? {
+        text: "last name is correct",
+        color: "green",
+      }
+    : {
+        text: "last name is not valid",
+        color: "red",
+      };
+}
+//////
+function loginValidation(value) {
+  const hasNumbers = /\d/; // ищет любую цифру
+  return value.length >= 6 && value.length <= 24 && !hasNumbers.test(value)
+    ? {
+        text: "login is correct",
+        color: "green",
+      }
+    : {
+        text: "login is not valid",
+        color: "red",
+      };
+}
+///
+function passwordValidation(value) {
+  const specialCharRegex = /[!@#$%^&*(),.?":{}|<>_\-+=~`[\]\\]/;
+
+  return value.length >= 8 && specialCharRegex.test(value)
+    ? {
+        text: "password is correct",
+        color: "red",
+      }
+    : {
+        text: "minimum 8 characters and 1 special character",
+        color: "green",
+      };
+}
+//////
+usernameInput.addEventListener("input", (event) => {
+  const statusData = usernameValidation(event.target.value);
+  renderStatusMessage(usernameLabel, statusData);
+});
+
+lastnameInput.addEventListener("input", (event) => {
+  const statusData = lastnameValidation(event.target.value);
+  renderStatusMessage(lastnameLabel, statusData);
+});
+//////
+loginInput.addEventListener("input", (event) => {
+  const statusData = loginValidation(event.target.value);
+  renderStatusMessage(loginLabel, statusData);
+});
+
+passwordInput.addEventListener("input", (event) => {
+  const statusData = passwordValidation(event.target.value);
+  renderStatusMessage(passwordLabel, statusData);
+});
+////////
+///////////////////////////////////////////////////////////
